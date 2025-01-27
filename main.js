8500b53e58348f1b64e7126ad209bbdd780e6e24
Ebrahiem Francis <ebrahiemfrancis741@gmail.com>
2025-01-27 04:37:16 +0200

    Add mergeSort function

--- main.js
+++ main.js
@@ -19,4 +19,34 @@ function fibsRec(n) {
   }
 }
 
-console.log(fibsRec(10));
+
+/*
+
+*/
+function mergeSort(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+  
+  const left = mergeSort(arr.slice(0, Math.floor(arr.length / 2)));
+  console.log('left:')
+  console.log(left);
+  const right = mergeSort(arr.slice(Math.floor(arr.length / 2)));
+  console.log('right:');
+  console.log(right);
+  const sorted = [];
+  let i = 0,
+    j = 0;
+
+  while (i < left.length && j < right.length) {
+    if (left[i] < right[j]) {
+      sorted.push(left[i++]);
+    } else {
+      sorted.push(right[j++]);
+    }
+  }
+
+  return sorted.concat(left.slice(i), right.slice(j));
+}
+
+console.log(mergeSort([676, 10, 937, 105, 181]));
